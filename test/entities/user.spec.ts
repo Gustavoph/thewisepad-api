2ab0a125c87ff1aec768f3f86fcbcd285176295b
Gustavo Oliveira <gustavo.gabriel@beresolution.com>
2022-08-31 18:47:36 -0300

    test: ensure User return an new user if provided valid data

--- test/entities/user.spec.ts
+++ test/entities/user.spec.ts
@@ -9,22 +9,28 @@ const emptyPassword = ''
 
 describe('User domain entity', () => {
   test('should not create user if invalid email is provided', () => {
-    const error = User.create(invalidEmail, validPassword)
+    const error = User.create(invalidEmail, validPassword).value as Error
     expect(error.name).toEqual('InvalidEmailError')
   })
 
   test('should not create user if invalid password is provided (no numbers)', () => {
-    const error = User.create(validEmail, invalidPasswordWithNoNumbers)
+    const error = User.create(validEmail, invalidPasswordWithNoNumbers).value as Error
     expect(error.name).toEqual('InvalidPasswordError')
   })
 
   test('should not create user if invalid password is provided (too few chars)', () => {
-    const error = User.create(validEmail, invalidPasswordWithTooFewCharacters)
+    const error = User.create(validEmail, invalidPasswordWithTooFewCharacters).value as Error
     expect(error.name).toEqual('InvalidPasswordError')
   })
 
   test('should not create user if invalid password is provided (empty)', () => {
-    const error = User.create(validEmail, emptyPassword)
+    const error = User.create(validEmail, emptyPassword).value as Error
     expect(error.name).toEqual('InvalidPasswordError')
+  })
+
+  test('should create user if valid email and password is provided', () => {
+    const user = User.create(validEmail, validPassword).value as User
+    expect(user.email.value).toEqual(validEmail)
+    expect(user.password.value).toEqual(validPassword)
   })
 })
