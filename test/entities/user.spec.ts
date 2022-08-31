eb58951b3a9afc5c50a1b554927e670b31e8e633
Gustavo Oliveira <gustavo.gabriel@beresolution.com>
2022-08-31 18:23:08 -0300

    test: ensure User return InvalidPasswordError if provided an empty password

--- test/entities/user.spec.ts
+++ test/entities/user.spec.ts
@@ -5,7 +5,7 @@ const validPassword = '1validpassword'
 const invalidEmail = 'invalid_email'
 const invalidPasswordWithNoNumbers = 'invalid'
 const invalidPasswordWithTooFewCharacters = '123ab'
-// const emptyPassword = ''
+const emptyPassword = ''
 
 describe('User domain entity', () => {
   test('should not create user if invalid email is provided', () => {
@@ -20,6 +20,11 @@ describe('User domain entity', () => {
 
   test('should not create user if invalid password is provided (too few chars)', () => {
     const error = User.create(validEmail, invalidPasswordWithTooFewCharacters)
+    expect(error.name).toEqual('InvalidPasswordError')
+  })
+
+  test('should not create user if invalid password is provided (empty)', () => {
+    const error = User.create(validEmail, emptyPassword)
     expect(error.name).toEqual('InvalidPasswordError')
   })
 })
