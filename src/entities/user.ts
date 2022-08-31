3cb6a0cf674edfa5efa4380e6a36e9fe9ccc3c21
Gustavo Oliveira <gustavo.gabriel@beresolution.com>
2022-08-31 18:47:27 -0300

    feat: ensure User return an new user if provided valid data

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,17 +1,40 @@
-import { left } from '@/shared'
 import { Email, Password } from '@/entities'
-import { InvalidEmailError, InvalidPasswordError } from './errors'
+import { Either, left, right } from '@/shared'
+import { InvalidEmailError, InvalidPasswordError } from '@/entities/errors'
 
 export class User {
-  public static create (email: string, password: string): InvalidEmailError | InvalidPasswordError {
+  private readonly _email: Email
+  private readonly _password: Password
+
+  public get email () {
+    return this._email
+  }
+
+  public get password () {
+    return this._password
+  }
+
+  constructor (email: Email, password: Password) {
+    this._email = email
+    this._password = password
+    Object.freeze(this)
+  }
+
+  public static create (email: string, password: string):
+  Either<InvalidEmailError | InvalidPasswordError, User> {
     const emailOrError = Email.create(email)
     if (emailOrError.isLeft()) {
-      return new InvalidEmailError(email)
+      return left(new InvalidEmailError(email))
     }
 
     const passwordOrError = Password.create(password)
     if (passwordOrError.isLeft()) {
-      return new InvalidPasswordError()
+      return left(new InvalidPasswordError())
     }
+
+    const emailObject = emailOrError.value
+    const passwordObject = passwordOrError.value
+
+    return right(new User(emailObject, passwordObject))
   }
 }
