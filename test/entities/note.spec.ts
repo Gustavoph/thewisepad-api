de6f38cff4df1b8f9f4befcf262af0b6108ab6a5
Gustavo Oliveira <gustavo.gabriel@beresolution.com>
2022-08-31 20:33:49 -0300

    test: should create a note with provided valid data

--- test/entities/note.spec.ts
+++ test/entities/note.spec.ts
@@ -2,7 +2,7 @@
 import { User, Note } from '@/entities'
 
 const invalidTitle = ''
-// const validTitle = 'my note'
+const validTitle = 'my note'
 const validEmail = 'my@mail.com'
 const validPassword = '1validpassword'
 const validContent = 'content'
@@ -10,7 +10,14 @@ const validOwner: User = User.create(validEmail, validPassword).value as User
 
 describe('Note entity', () => {
   test('should not be created with invalid title (is empty)', () => {
-    const error = Note.create(validOwner, invalidTitle, validContent)
+    const error = Note.create(validOwner, invalidTitle, validContent).value as Error
     expect(error.name).toBe('InvalidTitleError')
+  })
+
+  test('should be created with a valid title and owner', () => {
+    const note: Note = Note.create(validOwner, validTitle, validContent).value as Note
+    expect(note.title.value).toBe('my note')
+    expect(note.owner.email.value).toBe('my@mail.com')
+    expect(note.content).toBe(validContent)
   })
 })
