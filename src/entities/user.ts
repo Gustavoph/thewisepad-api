f1c1d8a367366de4c015a1cb9b02ed9054fb489c
Gustavo Oliveira <gustavo.gabriel@beresolution.com>
2022-08-31 18:21:45 -0300

    feat: ensure User return Invalid Email Error if invalid email provided

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,41 +1,17 @@
-import { Either, left, right } from '../shared'
-import { Email } from './email'
+import { left } from '@/shared'
+import { Email, Password } from '@/entities'
 import { InvalidEmailError, InvalidPasswordError } from './errors'
-import { Password } from './password'
 
 export class User {
-  private readonly _email: Email
-  private readonly _password: Password
-
-  public get email (): Email {
-    return this._email
-  }
-
-  public get password (): Password {
-    return this._password
-  }
-
-  private constructor (email: Email, password: Password) {
-    this._email = email
-    this._password = password
-    Object.freeze(this)
-  }
-
-  public static create (email: string, password: string):
-  Either<InvalidEmailError | InvalidPasswordError, User> {
+  public static create (email: string, password: string): InvalidEmailError | InvalidPasswordError {
     const emailOrError = Email.create(email)
     if (emailOrError.isLeft()) {
-      return left(new InvalidEmailError(email))
+      return new InvalidEmailError(email)
     }
 
     const passwordOrError = Password.create(password)
     if (passwordOrError.isLeft()) {
-      return left(new InvalidPasswordError())
+      return new InvalidPasswordError()
     }
-
-    const emailObject = emailOrError.value
-    const passwordObject = passwordOrError.value
-
-    return right(new User(emailObject, passwordObject))
   }
 }
