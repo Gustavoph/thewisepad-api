a0360eef1b940a15713688c4e4520de4272b383f
Gustavo Oliveira <gustavo.gabriel@beresolution.com>
2022-08-31 20:08:42 -0300

    test: ensure Note entity return InvalidTitleError if invalid title is Provided

--- test/entities/note.spec.ts
+++ test/entities/note.spec.ts
@@ -1,5 +1,16 @@
-describe('Note domain entity', () => {
-  test('', () => {
-    expect(1).toBe(1)
+
+import { User, Note } from '@/entities'
+
+const invalidTitle = ''
+// const validTitle = 'my note'
+const validEmail = 'my@mail.com'
+const validPassword = '1validpassword'
+const validContent = 'content'
+const validOwner: User = User.create(validEmail, validPassword).value as User
+
+describe('Note entity', () => {
+  test('should not be created with invalid title (is empty)', () => {
+    const error = Note.create(validOwner, invalidTitle, validContent)
+    expect(error.name).toBe('InvalidTitleError')
   })
 })
